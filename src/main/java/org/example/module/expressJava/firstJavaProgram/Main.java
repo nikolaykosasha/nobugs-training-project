4c69316f19f1bbbd7e01d278ba3420975d5c18b9
Nikolayko Aleksander <octopus-pillow.09@icloud.com>
2026-04-08 20:22:46 +0300

    update structure

--- src/main/java/org/example/module/expressJava/firstJavaProgram/Main.java
+++ src/main/java/org/example/module/expressJava/firstJavaProgram/Main.java
@@ -1,4 +1,4 @@
-package org.example.HW_FirstJavaProgram;
+package org.example.module.expressJava.firstJavaProgram;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/org/example/module/expressJava/firstJavaProgram/MathOperations.java
+++ src/main/java/org/example/module/expressJava/firstJavaProgram/MathOperations.java
@@ -1,4 +1,4 @@
-package org.example.HW_FirstJavaProgram;
+package org.example.module.expressJava.firstJavaProgram;
 
 public class MathOperations {
 
